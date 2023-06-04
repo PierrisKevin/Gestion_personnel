2d8abdc651cb3a99f6ded91c8053afa6caff6baa
Pierris kevin <krandy018@gmail.com>
2023-06-04 05:03:10 +0300

    debut de creation des composant

---
+++ Gestion_chambre/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace Gestion_chambre
+{
+    partial class main_views
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(main_views));
+            this.sieBars_contain = new System.Windows.Forms.Panel();
+            this.facture_link = new System.Windows.Forms.Button();
+            this.reserv_link = new System.Windows.Forms.Button();
+            this.room_link = new System.Windows.Forms.Button();
+            this.client_link = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.headear = new System.Windows.Forms.Panel();
+            this.exit_btn = new System.Windows.Forms.Button();
+            this.sieBars_contain.SuspendLayout();
+            this.headear.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // sieBars_contain
+            // 
+            this.sieBars_contain.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(30)))), ((int)(((byte)(40)))));
+            this.sieBars_contain.Controls.Add(this.facture_link);
+            this.sieBars_contain.Controls.Add(this.reserv_link);
+            this.sieBars_contain.Controls.Add(this.room_link);
+            this.sieBars_contain.Controls.Add(this.client_link);
+            this.sieBars_contain.Controls.Add(this.button1);
+            this.sieBars_contain.Dock = System.Windows.Forms.DockStyle.Left;
+            this.sieBars_contain.Location = new System.Drawing.Point(0, 0);
+            this.sieBars_contain.Name = "sieBars_contain";
+            this.sieBars_contain.Size = new System.Drawing.Size(243, 813);
+            this.sieBars_contain.TabIndex = 0;
+            // 
+            // facture_link
+            // 
+            this.facture_link.FlatAppearance.BorderSize = 0;
+            this.facture_link.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.facture_link.ForeColor = System.Drawing.Color.White;
+            this.facture_link.Image = ((System.Drawing.Image)(resources.GetObject("facture_link.Image")));
+            this.facture_link.Location = new System.Drawing.Point(3, 321);
+            this.facture_link.Name = "facture_link";
+            this.facture_link.Size = new System.Drawing.Size(216, 49);
+            this.facture_link.TabIndex = 4;
+            this.facture_link.Text = "facture";
+            this.facture_link.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.facture_link.UseVisualStyleBackColor = true;
+            // 
+            // reserv_link
+            // 
+            this.reserv_link.FlatAppearance.BorderSize = 0;
+            this.reserv_link.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.reserv_link.ForeColor = System.Drawing.Color.White;
+            this.reserv_link.Image = ((System.Drawing.Image)(resources.GetObject("reserv_link.Image")));
+            this.reserv_link.Location = new System.Drawing.Point(12, 266);
+            this.reserv_link.Name = "reserv_link";
+            this.reserv_link.Size = new System.Drawing.Size(216, 49);
+            this.reserv_link.TabIndex = 3;
+            this.reserv_link.Text = " reservation";
+            this.reserv_link.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.reserv_link.UseVisualStyleBackColor = true;
+            // 
+            // room_link
+            // 
+            this.room_link.FlatAppearance.BorderSize = 0;
+            this.room_link.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.room_link.ForeColor = System.Drawing.Color.White;
+            this.room_link.Image = ((System.Drawing.Image)(resources.GetObject("room_link.Image")));
+            this.room_link.Location = new System.Drawing.Point(12, 211);
+            this.room_link.Name = "room_link";
+            this.room_link.Size = new System.Drawing.Size(216, 49);
+            this.room_link.TabIndex = 2;
+            this.room_link.Text = " chambre";
+            this.room_link.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.room_link.UseVisualStyleBackColor = true;
+            // 
+            // client_link
+            // 
+            this.client_link.FlatAppearance.BorderSize = 0;
+            this.client_link.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.client_link.ForeColor = System.Drawing.Color.White;
+            this.client_link.Image = ((System.Drawing.Image)(resources.GetObject("client_link.Image")));
+            this.client_link.Location = new System.Drawing.Point(3, 156);
+            this.client_link.Name = "client_link";
+            this.client_link.Size = new System.Drawing.Size(216, 49);
+            this.client_link.TabIndex = 1;
+            this.client_link.Text = " client";
+            this.client_link.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.client_link.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(10, 101);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(216, 49);
+            this.button1.TabIndex = 0;
+            this.button1.Text = " dashboard";
+            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // headear
+            // 
+            this.headear.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.headear.Controls.Add(this.exit_btn);
+            this.headear.Dock = System.Windows.Forms.DockStyle.Top;
+            this.headear.Location = new System.Drawing.Point(243, 0);
+            this.headear.Name = "headear";
+            this.headear.Size = new System.Drawing.Size(1122, 51);
+            this.headear.TabIndex = 1;
+            // 
+            // exit_btn
+            // 
+            this.exit_btn.FlatAppearance.BorderSize = 0;
+            this.exit_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.exit_btn.Image = ((System.Drawing.Image)(resources.GetObject("exit_btn.Image")));
+            this.exit_btn.Location = new System.Drawing.Point(1050, 12);
+            this.exit_btn.Name = "exit_btn";
+            this.exit_btn.Size = new System.Drawing.Size(69, 36);
+            this.exit_btn.TabIndex = 0;
+            this.exit_btn.UseVisualStyleBackColor = true;
+            // 
+            // main_views
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1365, 813);
+            this.Controls.Add(this.headear);
+            this.Controls.Add(this.sieBars_contain);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "main_views";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "main_views";
+            this.sieBars_contain.ResumeLayout(false);
+            this.headear.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Panel sieBars_contain;
+        private Panel headear;
+        private Button button1;
+        private Button facture_link;
+        private Button reserv_link;
+        private Button room_link;
+        private Button client_link;
+        private Button exit_btn;
+    }
+}
---
+++ Gestion_chambre/Form1.cs
@@ -0,0 +1,10 @@
+namespace Gestion_chambre
+{
+    public partial class main_views : Form
+    {
+        public main_views()
+        {
+            InitializeComponent();
+        }
+    }
+}
